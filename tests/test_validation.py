8b999df16e7a8bf1e075adede7e0cebeb829a636
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2023-02-13 17:19:00 +0100

    move existing tags

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -13,14 +13,14 @@ def get_schema():
         return json.load(f)
 
 
-@pytest.mark.parametrize('file', Path(Path().root).glob('*.json'))
+@pytest.mark.parametrize('file', Path(Path(Path().root, 'tokens')).glob('*.json'))
 def test_token_list_schema(file, get_schema):
     with open(file) as f:
         token_list = json.load(f)
         validate(token_list, get_schema)
 
 
-@pytest.mark.parametrize('file', Path(Path().root).glob('*.json'))
+@pytest.mark.parametrize('file', Path(Path(Path().root, 'tokens')).glob('*.json'))
 def test_token_tag_in_tags_list(file):
     """Check that all tokens in the list have a tag in the tags list."""
     with open(file) as f:
